1b309b890a8821b81f3c82697201c59c8c4b74ec
Renato Oliveira <renatorro@comp.ufla.br>
2016-04-28 14:34:19 -0300

    Pagination

--- backend-java/src/main/frontend/jsx/core/view/system/companies/Companies.jsx
+++ backend-java/src/main/frontend/jsx/core/view/system/companies/Companies.jsx
@@ -1,19 +1,26 @@
 
 import React from "react";
 
+import CompanyStore from "forpdi/jsx/core/store/Company.jsx";
+
+import LoadingGauge from "forpdi/jsx/core/widget/LoadingGauge.jsx";
 import Modal from "forpdi/jsx/core/widget/Modal.jsx";
+import Pagination from "forpdi/jsx/core/widget/Pagination.jsx";
 
 export default React.createClass({
 	getInitialState() {
 		return {
+			loading: true
 		};
 	},
 	componentDidMount() {
 		var me = this;
-		
+		CompanyStore.on('sync', store => {
+			this.setState({loading: false});
+		}, me);
 	},
 	componentWillUnmount() {
-		
+		CompanyStore.off(null, null, this);
 	},
 
 	onTabClick(tab) {
@@ -23,6 +30,12 @@ export default React.createClass({
 	render() {
 		return (<div className="container-fluid animated fadeIn">
 			<h1>Instituições</h1>
+			{this.state.loading ? <LoadingGauge />:
+				<div>
+					
+				</div>
+			}
+			<Pagination store={CompanyStore} />
 		</div>);
 	  }
 	});
