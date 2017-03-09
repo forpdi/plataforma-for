0a7f4910ca990e3b54983d92af60301eb8ae1063
isalvati <igorsalvati@gmail.com>
2017-03-09 15:14:42 -0300

    Troca de diretorio do PDF

--- backend-java/src/main/java/org/forpdi/planning/document/DocumentBS.java
+++ backend-java/src/main/java/org/forpdi/planning/document/DocumentBS.java
@@ -871,7 +871,7 @@ public class DocumentBS extends HibernateBusiness {
 
 		ClassLoader classLoader = getClass().getClassLoader();
 		String resourcesPath = new File(classLoader.getResource("/reports/pdf/example.pdf").getFile()).getPath();
-		// resourcesPath = "/tmp"; // corrigir para salvar com um caminho
+		resourcesPath = "/tmp"; // corrigir para salvar com um caminho
 		// dinamico
 		resourcesPath = resourcesPath.replace("example.pdf", "");
 		resourcesPath = resourcesPath.replace("%20", " ");
@@ -1006,7 +1006,7 @@ public class DocumentBS extends HibernateBusiness {
 			} else { // SEÇÕES NUMERADAS
 				if (lastSecWasPreText) {
 					lastSecWasPreText = false;
-					LOGGER.info("SUMARIO");
+					//LOGGER.info("SUMARIO");
 				} else {
 
 					secIndex++;
@@ -1085,7 +1085,7 @@ public class DocumentBS extends HibernateBusiness {
 										classLoader.getResource("/reports/html/example.html").getFile()).getPath();
 								resourcesPath = resourcesPath.replace("example.html", "");
 								resourcesPath = resourcesPath.replace("%20", " ");
-								// resourcesPath = "/tmp"; // corrigir para usar
+								resourcesPath = "/tmp"; // corrigir para usar
 								// um caminho
 								// dinamico
 								File htmlFile = File.createTempFile("output.", ".html", new File(resourcesPath));
@@ -1326,7 +1326,7 @@ public class DocumentBS extends HibernateBusiness {
 														.getPath();
 										resourcesPath = resourcesPath.replace("example.html", "");
 										resourcesPath = resourcesPath.replace("%20", " ");
-										// resourcesPath = "/tmp"; // corrigir
+										resourcesPath = "/tmp"; // corrigir
 										// para
 										// usar
 										// um caminho
@@ -2124,7 +2124,7 @@ public class DocumentBS extends HibernateBusiness {
 
 		ClassLoader classLoader = getClass().getClassLoader();
 		String resourcesPath = new File(classLoader.getResource("/reports/pdf/example.pdf").getFile()).getPath();
-		// resourcesPath = "/tmp"; // corrigir para salvar com um caminho
+		resourcesPath = "/tmp"; // corrigir para salvar com um caminho
 		// dinamico
 		resourcesPath = resourcesPath.replace("example.pdf", "");
 		resourcesPath = resourcesPath.replace("%20", " ");
