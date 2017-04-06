c305ca104d9babba40965b5accae4a00a5242435
Renato Oliveira <renatorro@comp.ufla.br>
2017-04-06 13:50:54 -0300

    Parametrizando melhor os emails.

--- backend-java/src/main/java/org/forpdi/system/EmailUtilsPlugin.java
+++ backend-java/src/main/java/org/forpdi/system/EmailUtilsPlugin.java
@@ -24,14 +24,14 @@ public class EmailUtilsPlugin {
 	public static String sendSimpleEmail(String toEmail, String toName, String subject, String msg) throws EmailException {
 		Email email = EmailUtils.getSimpleEmail();
 		email.setAuthentication(SystemConfigs.getConfig("smtp.user"), SystemConfigs.getConfig("smtp.password"));
-		email.setFrom("noreply@forpdi.org", "ForPDI");
+		email.setFrom(SystemConfigs.getConfig("smtp.from.email"), SystemConfigs.getConfig("smtp.from.name"));
 		email.setHostName(SystemConfigs.getConfig("smtp.host"));
 		email.setSmtpPort(Integer.parseInt(SystemConfigs.getConfig("smtp.port")));
-		email.setSSLOnConnect(false);
 		email.addTo(toEmail, toName);
 		email.setSubject(subject);
 		email.setMsg(msg);
-		email.setStartTLSEnabled(true);
+		email.setSSLOnConnect("true".equals(SystemConfigs.getConfig("smtp.ssl")));
+		email.setStartTLSEnabled("true".equals(SystemConfigs.getConfig("smtp.tls")));
 		//email.setStartTLSRequired(true);
 		return email.send();
 	}
@@ -51,14 +51,14 @@ public class EmailUtilsPlugin {
 	public static String sendHtmlEmail(String toEmail, String toName, String subject, String msg) throws EmailException {
 		Email email = EmailUtils.getHtmlEmail();
 		email.setAuthentication(SystemConfigs.getConfig("smtp.user"), SystemConfigs.getConfig("smtp.password"));
-		email.setFrom("noreply@forpdi.org", "ForPDI");
+		email.setFrom(SystemConfigs.getConfig("smtp.from.email"), SystemConfigs.getConfig("smtp.from.name"));
 		email.setHostName(SystemConfigs.getConfig("smtp.host"));
 		email.setSmtpPort(Integer.parseInt(SystemConfigs.getConfig("smtp.port")));
-		email.setSSLOnConnect(false);
 		email.addTo(toEmail, toName);
 		email.setSubject(subject);
 		email.setMsg(msg);
-		email.setStartTLSEnabled(true);
+		email.setSSLOnConnect("true".equals(SystemConfigs.getConfig("smtp.ssl")));
+		email.setStartTLSEnabled("true".equals(SystemConfigs.getConfig("smtp.tls")));
 		//email.setStartTLSRequired(true);
 		return email.send();
 	}
