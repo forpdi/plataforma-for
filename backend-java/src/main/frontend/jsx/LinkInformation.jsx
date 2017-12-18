286c388ca1de26c84945e3995d80838a858bfeee
Rafael_Lima <rafaelsplima@gmail.com>
2017-12-18 09:40:25 -0200

    AJustes Menu: Ajuda

--- backend-java/src/main/frontend/jsx/LinkInformation.jsx
+++ backend-java/src/main/frontend/jsx/LinkInformation.jsx
@@ -6,11 +6,19 @@ export default React.createClass({
     render() {
         return (
             <div>
-                <span className ="fpdi-nav-label fontSize12"> Portal ForPDI: http://forpdi.org/ </span>
-                <br /> 
-				<span className ="fpdi-nav-label fontSize12"> Download do Livro: http://forpdi.org/... </span>
-				<span className ="fpdi-nav-label fontSize12 white-space"> Capacitação: https://www.youtube.com/channel/UC1DP0ZGoTm0OlX-vOc-Ns5Q/videos </span>
-				<span className ="fpdi-nav-label fontSize12"> Manual do usuário: http://forpdi.org/manual.pdf </span>
+                <a href="http://forpdi.org/" target="_blanck">
+                    <span className ="fpdi-nav-label fontSize12"> Portal ForPDI </span>
+                </a>
+                <br />
+                <a href="http://forpdi.org/" target="_blanck">
+				    <span className ="fpdi-nav-label fontSize12"> Download do Livro </span>
+                </a>
+                <a href="https://www.youtube.com/channel/UC1DP0ZGoTm0OlX-vOc-Ns5Q/videos" target="_blanck">
+				    <span className ="fpdi-nav-label fontSize12"> Capacitação </span>
+                </a>
+                <a href="http://forpdi.org/manual.pdf" target="_blanck">
+				    <span className ="fpdi-nav-label fontSize12"> Manual do usuário </span>
+                </a>
             </div>
         );
     }
