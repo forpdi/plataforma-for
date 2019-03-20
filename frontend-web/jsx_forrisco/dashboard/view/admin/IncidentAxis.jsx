cb70ec4e04df7d3cf2f2c5b9c66f525b97c5f3d3
Juliano Afonso <julianoafonsso1@gmail.com>
2019-03-20 14:16:23 -0300

    ISSUE#191 Correção no gráfico de ameaças e oportunidades

--- frontend-web/jsx_forrisco/dashboard/view/admin/IncidentAxis.jsx
+++ frontend-web/jsx_forrisco/dashboard/view/admin/IncidentAxis.jsx
@@ -15,16 +15,16 @@ export default React.createClass({
 	getInitialState() {
 		return {
 			plan: null,
-			units:[],
-			incidents:[],
-			unit:-1,
+			units: [],
+			incidents: [],
+			unit: -1,
 			loading: true,
 			year: [],
 			data: [],
-			thematicAxes:[],
+			thematicAxes: [],
 			selectedThematicAxes: -1,
-			options:{
-				hAxis: {title: "Tempo", minValue: 0, maxValue: 12, },
+			options: {
+				hAxis: {title: "Tempo", minValue: 0, maxValue: 12,},
 				vAxis: {title: 'Quantidade', minValue: 0, maxValue: 15},
 				legend: {position: 'none'},
 				explorer: {axis: 'horizontal'},
@@ -32,8 +32,8 @@ export default React.createClass({
 			},
 			chartEvents: [
 				{
-					eventName : 'select',
-					callback  : this.onChartClick
+					eventName: 'select',
+					callback: this.onChartClick
 				},
 			],
 			pageSize: 10,
@@ -41,62 +41,62 @@ export default React.createClass({
 			threats: true,
 		};
 	},
-	uniques(array){
+
+	uniques(array) {
 		return array.filter(function (value, index, self) {
-		  return self.indexOf(value) === index;
+			return self.indexOf(value) === index;
 		});
 	},
+
 	componentDidMount() {
 
 		var me = this;
 
-		RiskStore.on("incidentbByPlan",(model) =>{
+		RiskStore.on("incidentbByPlan", (model) => {
+			this.state.incidents = [];
 
-			this.state.incidents=[]
-
-			for(var i=0;i< model.data.length;i++){
-				this.state.incidents.push(model.data[i])
-				var data=moment(model.data[i].begin,'DD/MM/YYYY hh:mm:ss').toDate();
+			for (var i = 0; i < model.data.length; i++) {
+				this.state.incidents.push(model.data[i]);
+				var data = moment(model.data[i].begin, 'DD/MM/YYYY hh:mm:ss').toDate();
 				this.state.year.push(data.getFullYear())
 			}
 
-			this.state.year=this.uniques(this.state.year).sort().reverse()
+			this.state.year = this.uniques(this.state.year).sort().reverse();
 
 			this.setState({
-				incidents:this.state.incidents,
-				year:this.state.year,
-				loading:false
-			})
+				incidents: this.state.incidents,
+				year: this.state.year,
+				loading: false
+			});
 
-			this.state.unit=-1
+			this.state.unit = -1;
 
-			this.LoadIncidents((new Date).getFullYear(),this.state.unit)
-		},me);
-
+			this.LoadIncidents((new Date).getFullYear(), this.state.unit)
+		}, me);
 	},
+
 	componentWillUnmount() {
 		RiskStore.off(null, null, this);
 	},
 
-	componentWillReceiveProps(newProps){
-		var me = this;
-		this.state.plan=newProps.plan
-		this.state.units=newProps.units
+	componentWillReceiveProps(newProps) {
+		this.state.plan = newProps.plan;
+		this.state.units = newProps.units;
+
 		this.setState({
 			plan: newProps.plan,
 			units: newProps.units,
 			loading: true,
 		});
 
+		this.state.year = [(new Date).getFullYear()];
 
-		this.state.year=[(new Date).getFullYear()]
 
-
-		if(newProps.units.length ==0){
-			this.state.incidents=[]
-			this.state.unit=-1
-			this.LoadIncidents((new Date).getFullYear(),this.state.unit)
-		}else{
+		if (newProps.units.length == 0) {
+			this.state.incidents = [];
+			this.state.unit = -1;
+			this.LoadIncidents((new Date).getFullYear(), this.state.unit)
+		} else {
 			RiskStore.dispatch({
 				action: RiskStore.ACTION_FIND_INCIDENTS_BY_PLAN,
 				data: newProps.plan.id
@@ -105,35 +105,35 @@ export default React.createClass({
 
 	},
 
-	onThematicAxesChange(){
+	onThematicAxesChange() {
 		this.getInfo(1, this.state.pageSize);
 	},
 
-	onChartClick(Chart){
+	onChartClick(Chart) {
 		var incidents = [];
 
-		for(var i in this.state.incidents) {
-			if(moment(this.state.incidents[i].begin,'DD/MM/YYYY hh:mm:ss').toDate().getFullYear() == this.refs['selectYear'].value) {
-				if(moment(this.state.incidents[i].begin,'DD/MM/YYYY hh:mm:ss').toDate().getMonth() == Chart.chart.getSelection()[0].row){
-					if(this.state.incidents[i].type == Chart.chart.getSelection()[0].column) {
+		for (var i in this.state.incidents) {
+			if (moment(this.state.incidents[i].begin, 'DD/MM/YYYY hh:mm:ss').toDate().getFullYear() == this.refs['selectYear'].value) {
+				if (moment(this.state.incidents[i].begin, 'DD/MM/YYYY hh:mm:ss').toDate().getMonth() == Chart.chart.getSelection()[0].row) {
+					if (this.state.incidents[i].type == Chart.chart.getSelection()[0].column) {
 						incidents.push(this.state.incidents[i])
 					}
 				}
 			}
 		}
 
-		if(Chart.chart.getSelection().length > 0) {
+		if (Chart.chart.getSelection().length > 0) {
 			Modal.incidentList(incidents);
 		}
 		Chart.chart.setSelection(false);
 	},
 
-	getInfo(page, pageSize, opt){
+	getInfo(page, pageSize, opt) {
 
 	},
 
-	onOpportunitiesChange(){
-		if(!this.state.threats && this.state.opportunities){
+	onOpportunitiesChange() {
+		if (!this.state.threats && this.state.opportunities) {
 			return
 		}
 
@@ -146,10 +146,10 @@ export default React.createClass({
 		this.onUnitChange(null)
 	},
 
-	onThreatsChange(){
+	onThreatsChange() {
 
-		if(this.state.threats && !this.state.opportunities){
-				return
+		if (this.state.threats && !this.state.opportunities) {
+			return
 		}
 
 		this.state.threats = !this.state.threats
@@ -161,120 +161,144 @@ export default React.createClass({
 		this.onUnitChange(null)
 	},
 
-	onUnitChange(evnt){
-		this.state.unit=this.refs['selectUnits'].value
-		this.LoadIncidents(this.refs['selectYear'].value,this.state.unit)
+	onUnitChange(evnt) {
+		this.state.unit = this.refs['selectUnits'].value
+		this.LoadIncidents(this.refs['selectYear'].value, this.state.unit)
 	},
 
-	onYearChange(){
+	onYearChange() {
 		this.onUnitChange();
 	},
 
-	LoadIncidents(year, unit){
+	LoadIncidents(year, unit) {
 
-		var data=[]
-		var incidents=[]
-		var mes={0:'jan',1:'fev',2:'mar',3:'abr',4:'mai',5:'jun',6:'jul',7:'ago',8:'set',9:'out',10:'nov',11:'dez'}
-		var month_thr={0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0}
-		var month_opp={0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0}
+		var data = []
+		var incidents = []
+		var mes = {
+			0: 'jan',
+			1: 'fev',
+			2: 'mar',
+			3: 'abr',
+			4: 'mai',
+			5: 'jun',
+			6: 'jul',
+			7: 'ago',
+			8: 'set',
+			9: 'out',
+			10: 'nov',
+			11: 'dez'
+		};
 
-		for(var i=0; i< this.state.incidents.length;i++){
-			if(moment(this.state.incidents[i].begin,'DD/MM/YYYY hh:mm:ss').toDate().getFullYear() == year){
-				if(unit == -1){
-					incidents.push(this.state.incidents[i])
-				}else{
-					if(unit== this.state.incidents[i].unitId){
+		var month_thr = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0};
+		var month_opp = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0};
+
+		for (var i = 0; i < this.state.incidents.length; i++) {
+			if (moment(this.state.incidents[i].begin, 'DD/MM/YYYY hh:mm:ss').toDate().getFullYear() == year) {
+				if (unit == -1) {
+					incidents.push(this.state.incidents[i])        //"Exibir Todas" selecionado
+				} else {
+					if (unit == this.state.incidents[i].unitId) {  //"Qualquer outra unidade" selecionada
 						incidents.push(this.state.incidents[i])
 					}
 				}
 			}
 		}
 
-		for(i=0;i<incidents.length;i++){
-			var this_month=moment(incidents[i].begin,'DD/MM/YYYY hh:mm:ss').toDate().getMonth()
+		for (i = 0; i < incidents.length; i++) {
+			//Converte mês para INT
+			var this_month = moment(incidents[i].begin, 'DD/MM/YYYY hh:mm:ss').toDate().getMonth();
 
-			if(incidents[i].type==1){
-				month_thr[this_month]+=1
-			}else{
-				month_opp[this_month]+=1
+			if (incidents[i].type == 1) {
+				month_thr[this_month] += 1 //Meses de ameaça
+			} else {
+				month_opp[this_month] += 1 //Meses de oportunidade
 			}
 		}
 
-		var month
-		if(year==(new Date).getFullYear()){
-			month=(new Date).getMonth()
-		}else{
-			month=12
+		var month;
+		if (year == (new Date).getFullYear()) {
+			month = (new Date).getMonth()
+		} else {
+			month = 12
 		}
 
 
-		var max=0
-		var axis=[]
-		if(this.state.opportunities && this.state.threats){
+		var max = 0;
+		var axis = [];
+		if (this.state.opportunities && this.state.threats) {
 			axis.push(['mes', 'ameaças', 'oportunidades'])
-			for(var i=0;i<month;i++){
-				axis.push([mes[i], month_thr[i], month_opp[i]])
+			for (var i = 0; i <= month; i++) {
 
-				max=max<month_thr[i]? month_thr[i]: max
-				max=max<month_opp[i]? month_opp[i]: max
+				axis.push([mes[i], month_thr[i], month_opp[i]]);
+				max = max < month_thr[i] ? month_thr[i] : max;
+				max = max < month_opp[i] ? month_opp[i] : max;
 
 			}
-		}else if(this.state.opportunities){
+		} else if (this.state.opportunities) {
 			axis.push(['mes', 'oportunidades'])
-			for(var i=0;i<month;i++){
+			for (var i = 0; i <= month; i++) {
 				axis.push([mes[i], month_opp[i]])
-				max=max<month_opp[i]? month_opp[i]: max
+				max = max < month_opp[i] ? month_opp[i] : max
 			}
-		}else{
+		} else {
 			axis.push(['mes', 'ameaças'])
-			for(var i=0;i<month;i++){
+			for (var i = 0; i <= month; i++) {
 				axis.push([mes[i], month_thr[i]])
-				max=max<month_thr[i]? month_thr[i]: max
+				max = max < month_thr[i] ? month_thr[i] : max
 			}
 		}
 
 		var colors;
-		if(this.state.opportunities && this.state.threats){
-			colors=["red","blue"]
-		}else if(this.state.opportunities){
-			colors=["blue"]
-		}else{
-			colors=["red"]
+		if (this.state.opportunities && this.state.threats) {
+			colors = ["red", "blue"]
+		} else if (this.state.opportunities) {
+			colors = ["blue"]
+		} else {
+			colors = ["red"]
 		}
-		this.state.options.colors=colors
-		this.state.options.vAxis.maxValue=max
+		this.state.options.colors = colors
+		this.state.options.vAxis.maxValue = max
 
 		this.setState({
-			data:axis,
-			loading:false,
-			options:this.state.options,
+			data: axis,
+			loading: false,
+			options: this.state.options,
 
 		})
+
+		console.log(this.state.incidents);
+		console.log(this.state.data);
 	},
 
 	render() {
 		return (<div>
 			<div className="panel panel-default">
 				<div className="panel-heading dashboard-panel-title">
-					<b className="budget-graphic-title" title={Messages.get("label.incidents")}>{Messages.get("label.incidents").toUpperCase()}</b>
+					<b className="budget-graphic-title"
+					   title={Messages.get("label.incidents")}>{Messages.get("label.incidents").toUpperCase()}</b>
 					<span className="frisco-containerSelect"> {Messages.get("label.units")}
-					<select onChange={this.onUnitChange} className="form-control dashboard-select-box-graphs marginLeft10" ref="selectUnits">
-						<option value={-1} data-placement="right" title={Messages.get("label.viewAll_")}> {Messages.get("label.viewAll_")} </option>
-						{this.state.units.map((attr, idy) =>{
-							return(
-							<option  key={attr.id} value={attr.id} data-placement="right" title={attr.name}>
-									{(attr.name.length>20)?(string(attr.name).trim().substr(0, 20).concat("...").toString()):(attr.name)}
-							</option>);})
-						}
+						<select onChange={this.onUnitChange}
+								className="form-control dashboard-select-box-graphs marginLeft10" ref="selectUnits">
+						<option value={-1} data-placement="right"
+								title={Messages.get("label.viewAll_")}> {Messages.get("label.viewAll_")} </option>
+							{this.state.units.map((attr, idy) => {
+								return (
+									<option key={attr.id} value={attr.id} data-placement="right" title={attr.name}>
+										{(attr.name.length > 20) ? (string(attr.name).trim().substr(0, 20).concat("...").toString()) : (attr.name)}
+									</option>);
+							})
+							}
 					</select>
 					</span>
 				</div>
 				<br/>
 				<div style={{"textAlign": "center"}}>
-					<select onChange={this.onYearChange} className="form-control dashboard-select-box-graphs marginLeft10" ref="selectYear">
-						{this.state.year.map((attr, idx) =>{
-							return(
-							<option key={idx} value={attr} data-placement="right" title={attr}>{attr}</option>);})
+					<select onChange={this.onYearChange}
+							className="form-control dashboard-select-box-graphs marginLeft10" ref="selectYear">
+						{this.state.year.map((attr, idx) => {
+							return (
+								<option key={idx} value={attr} data-placement="right" title={attr}>{attr}</option>);
+						})
 						}
 					</select>
 				</div>
@@ -291,13 +315,17 @@ export default React.createClass({
 							pageSize={this.state.pageSize}
 							total={this.state.total}
 							onChangePage={this.getInfo}
-							chartEvents={this.state.chartEvents} />
+							chartEvents={this.state.chartEvents}/>
 						<div className="colaborator-goal-performance-legend">
 							<span onClick={this.onThreatsChange}>
-								<span className="legend-item icon-link"  ><input type="text" className={"icon-link marginLeft10 legend-risk "+(this.state.threats ? "threat" :"") } disabled/> {Messages.getEditable("label.risk.threats","fpdi-nav-label")}</span>
+								<span className="legend-item icon-link"><input type="text"
+																			   className={"icon-link marginLeft10 legend-risk " + (this.state.threats ? "threat" : "")}
+																			   disabled/> {Messages.getEditable("label.risk.threats", "fpdi-nav-label")}</span>
 							</span>
 							<span onClick={this.onOpportunitiesChange}>
-								<span className="legend-item icon-link" ><input type="text"  className={"icon-link marginLeft10 legend-risk "+(this.state.opportunities ? "opportunities" :"") } disabled/> {Messages.getEditable("label.risk.opportunities","fpdi-nav-label")}</span>
+								<span className="legend-item icon-link"><input type="text"
+																			   className={"icon-link marginLeft10 legend-risk " + (this.state.opportunities ? "opportunities" : "")}
+																			   disabled/> {Messages.getEditable("label.risk.opportunities", "fpdi-nav-label")}</span>
 							</span>
 						</div>
 					</div>
