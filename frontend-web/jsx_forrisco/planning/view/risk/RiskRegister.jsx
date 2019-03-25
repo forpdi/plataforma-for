af34fe91db672e7a7bd135d425caa9d9e9338dc2
Matheus Nascimento <mnascimento0392@gmail.com>
2019-03-25 16:36:01 -0300

    Close #209

--- frontend-web/jsx_forrisco/planning/view/risk/RiskRegister.jsx
+++ frontend-web/jsx_forrisco/planning/view/risk/RiskRegister.jsx
@@ -626,8 +626,8 @@ export default React.createClass({
 		}
 
 		for (var i = 0; i < this.state.activities; i++) {
-
-			grau.push(
+			var pa=[]
+			pa.push(
 				this.getPA(i).map((field, idx) => {
 					return (<HorizontalInput
 						name={field.name}
@@ -639,14 +639,14 @@ export default React.createClass({
 			)
 
 			if (i > 0) {
-				grau.push(<Link onClick={this.deleteActivity.bind(this, i)}>
+				pa.push(<span><Link onClick={this.deleteActivity.bind(this, i)}>
 					<span className="mdi mdi-delete cursorPointer" title={Messages.get("label.deleteActicity")}></span>
-				</Link>)
+				</Link></span>)
 			}
-			grau.push(<br />)
+			grau.push(<div style={{"display": "inline-flex"}}>{pa} <br/></div>)
 		}
 
-		return (<div>{grau}<br /></div>)
+		return (<div>{grau}<br/></div>)
 	},
 
 	deleteActivity(x, y) {
@@ -716,7 +716,9 @@ export default React.createClass({
 		data['tipology'] = document.getElementById("field-tipology").value
 		data['type'] = document.getElementById("field-type").value
 
-		data['user'] = { id: this.state.users[this.refs["field-1"].refs.user.refs["field-user"].selectedIndex-1].id }
+		if(this.refs["field-1"].refs.user.refs["field-user"].selectedIndex != 0){
+			data['user'] = { id: this.state.users[this.refs["field-1"].refs.user.refs["field-user"].selectedIndex-1].id }
+		}
 		data['unit'] = { id: this.props.params.unitId }
 
 		data["risk_pdi"] = this.state.risk_pdi
