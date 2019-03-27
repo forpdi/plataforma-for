80976db735365ce5112a25b16a404b8a9e314ed0
fillypenascimento <fillypesax@gmail.com>
2019-03-27 11:58:13 -0300

    fix #115

--- frontend-web/jsx/ForRisco.jsx
+++ frontend-web/jsx/ForRisco.jsx
@@ -3,12 +3,18 @@ import React from "react";
 
 import MainMenu from "forpdi/jsx_forrisco/MainMenu.jsx";
 
+import Helmet from 'react-helmet';
+
 export default React.createClass({
 	render() {
 		return (
 			<div className="fpdi-app-body">
 				<MainMenu {...this.props} />
 				<div className="fpdi-app-content">
+					<Helmet>
+						<title>ForRisco</title>
+						<link rel="icon" type="image/x-icon" href="favicon2.ico"></link>
+					</Helmet>
 					{this.props.children}
 				</div>
 			</div>
